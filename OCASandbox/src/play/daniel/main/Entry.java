22409efe16c540aeef8aa7df87a3f4f51d7b6385
unknown <danielsmcelhaney@gmail.com>
2018-01-07 17:33:33 -0500

    initial push

---
+++ OCASandbox/src/play/daniel/main/Entry.java
@@ -0,0 +1,16 @@
+package play.daniel.main;
+
+public class Entry {
+
+	public static void main(String... args) {
+		// TODO Auto-generated method stub
+		if(args[0] != null) {
+			System.out.println(args[0]);
+		}
+		if(args[1] != null) {
+			System.out.println(args[1]);
+		}
+		
+	}
+
+}
