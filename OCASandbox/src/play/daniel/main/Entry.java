d7d28826c5c95ef7ddc484deccaedcc430c459d5
Daniel McElhaney <danielsmcelhaney@gmail.com>
2018-01-15 21:52:28 -0500

    added pre and postfix

--- OCASandbox/src/play/daniel/main/Entry.java
+++ OCASandbox/src/play/daniel/main/Entry.java
@@ -4,13 +4,20 @@ public class Entry {
 
 	public static void main(String... args) {
 		// TODO Auto-generated method stub
-		if(args[0] != null) {
+		/*if(args[0] != null) {
 			System.out.println(args[0]);
 		}
 		if(args[1] != null) {
 			System.out.println(args[1]);
 		}
-		
+		*/
+		System.out.println(PreAndPostfix());
+	}
+	
+	private static int PreAndPostfix() {
+		int a;
+		a = 10;
+		return a++ + a - a-- + a;
 	}
 
 }
